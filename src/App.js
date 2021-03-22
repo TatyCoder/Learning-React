f9b9053c1965938bf690b13d7539b513235eff23
TatyCoder <tmilanes@gmail.com>
2021-03-22 16:03:08 -0400

    Outputting lists

--- src/App.js
+++ src/App.js
@@ -3,29 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-// Adding a new property 'showPersons':
+// Adding an id:
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Sam', age: 29 },
-      { name: 'Lila', age: 27 }
+      { id: 'i1', name: 'Max', age: 28 },
+      { id: 'i2', name: 'Sam', age: 29 },
+      { id: 'i3', name: 'Lila', age: 27 }
     ],
     otherState: 'some other value',
     showPersons: false  
     // If it is false, I don't want to show the persons.
   }
 
-  switchNameHandler = (newName) => {
-    console.log('Was clicked!');
-    this.setState( {
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Sam', age: 29 },
-        { name: 'Lila', age: 27 }
-      ]
-    } )
-  }
+  // I'm not calling this handler anymore from anywhere in my application:
+  // switchNameHandler = (newName) => {
+  //   console.log('Was clicked!');
+  //   this.setState( {
+  //     persons: [
+  //       { name: newName, age: 28 },
+  //       { name: 'Sam', age: 29 },
+  //       { name: 'Lila', age: 27 }
+  //     ]
+  //   } )
+  // }
   
   nameChangedHandler = (event) => {
     this.setState( {
@@ -37,7 +38,14 @@ class App extends Component {
     } )
   }
 
-  // Creating a new method to switch the state:
+  // Adding this new handler:
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();  // Slice without arguments copies the array and returns a new one.
+    const persons = [...this.state.persons];  // To create a copy of persons array before manipulating it.
+    persons.splice(personIndex, 1);  // Then I create a new version of that persons array by splicing one element.
+    this.setState( {persons: persons} )  // Finally I call the updated array.
+  }
+ 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState( { showPersons: !doesShow } );  
@@ -54,29 +62,24 @@ class App extends Component {
       cursor: 'pointer'
     }
 
-    let persons = null;  //  If this is not true it will render nothing.
+    let persons = null;
 
-    // Adding a conditional, wrapping in a div all the content I want to show/hide:
-    if (this.state.showPersons) {  // If this is true it will render the persons.
-      persons = (
-        <div>
-        <Person 
-        name={this.state.persons[0].name} 
-        age={this.state.persons[0].age} />
-        <Person 
-        name={this.state.persons[1].name} 
-        age={this.state.persons[1].age}
-        click={this.switchNameHandler.bind(this, 'Bob!!!')} 
-        changed={this.nameChangedHandler}
-        > Hobbies: Surfing</Person>
-        <Person 
-        name={this.state.persons[2].name} 
-        age={this.state.persons[2].age} />
-      </div>
+    // Now outputting this as a list, wrapping in {} all the content to render it dynamically:
+    if (this.state.showPersons) {
+      persons = ( // map executes on every element of an array and returns an array that contains the results.
+        <div>  
+          {this.state.persons.map((person, index) => {
+            return <Person 
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age} 
+              key={person.id}  // The key property allows React to keep track of the individual elements.
+              />
+          })}
+        </div>
       );
     }
 
-    // Now I can output the persons variable here:
     return (
       <div className="App">
         <header className="App-header">
@@ -85,7 +88,6 @@ class App extends Component {
         </header>
         <button
           style={style}
-        // To show/hide persons, now passing 'togglePersonsHandler' as a reference:
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
         </div>
